a920e1102435ad0a5e694d86871f08c8b6909267
BrazilGabriel <gabriel.br1994@gmail.com>
2015-11-27 16:31:20 -0400

    finalizando os creditos

--- Wanderlust/js/credits.js
+++ Wanderlust/js/credits.js
@@ -8,8 +8,7 @@ var creditsState = {
     preload: function(){
     // Não há nenhum asset a ser carregado aqui, então a função fica vazia 
     this.game.load.bitmapFont('fipps', 'assets/fonts/fipps/fipps.png', 'assets/fonts/fipps/fipps.fnt');
-    this.game.load.bitmapFont('emulogic', 'assets/fonts/emulogic/emulogic.png', 'assets/fonts/emulogic/emulogic.fnt');
-    this.game.load.bitmapFont('lightemulogic', 'assets/fonts/lightemulogic/lightemulogic.png', 'assets/fonts/lightemulogic/lightemulogic.fnt');
+    this.game.load.bitmapFont('fippslight', 'assets/fonts/fipps/fippslight.png', 'assets/fonts/fipps/fippslight.fnt');
     this.game.load.spritesheet('back', 'assets/sprites/bg.png', 800, 600, 4);
 
     },   
@@ -21,25 +20,17 @@ var creditsState = {
     
     this.status = 0;
     
-    var style = { 
-        font: '28px fipps',
-        align: "center", // alinhamento do texto
-        fontSize: '28px'
-    }
-    
-    this.back = this.game.add.sprite(0,0,'back', 0);
-    this.back.animations.add('on', [0,1,2,3], 6.5);
+    this.game.stage.backgroundColor = "999999";
+
+   // this.back = this.game.add.sprite(0,0,'back', 0);
+  //  this.back.animations.add('on', [0,1,2,3], 6.5);
     //this.background = this.game.add.sprite(0,0);
-    //this.background.width = 800;
-	//this.background.height = 600;
-    //this.filter = this.game.add.filter('Fire', 800, 600);
-	//this.filter.alpha = 0.0;
-    //this.background.filters = [this.filter];
-    this.title = this.game.add.bitmapText(400, 100, 'fipps','WANDERLUST', 32);
+
+    this.title = this.game.add.bitmapText(400, 500, 'fipps','WANDERLUST', 32);
     this.title.anchor.setTo(0.5,0.5);
     this.title.generateTexture;
    
-    this.sub = this.game.add.bitmapText(400, 150, 'fipps','Credits', 28);
+    this.sub = this.game.add.bitmapText(400, 550, 'fipps','Credits', 28);
     this.sub.anchor.setTo(0.5,0.5);
     
     //this.art = this.game.add.bitmapText(400, 200, 'fipps','Artists', 14);
@@ -48,14 +39,13 @@ var creditsState = {
    // this.prog = this.game.add.bitmapText(400, 340, 'fipps','Programmers', 14);
    //this.prog.anchor.setTo(0.5,0.5);
     
-    this.team = '    DOUGLAS MATHEUS \n\n\n    VIRGINIA OLIVEIRA \n\n\n       GABRIEL ALONSO \n\n\n        GABRIEL BRASIL \n\n         BRUNO ARAUJO \n     CRISTINA ARAUJO \n            RAFAEL LIMA';
-    this.func = 'Concept\nElements\n\nConcept\nCharacters \n\nPhysics \nSound Design \n\nAnimations\nGame UI\n\nProgramming'   
-    this.text = this.game.add.bitmapText(400, 225, 'fipps', this.team, 18);
+    this.team = '    DOUGLAS MATHEUS \n\n\n    VIRGINIA OLIVEIRA \n\n\n       GABRIEL ALONSO \n\n\n        GABRIEL BRASIL \n\n\n         BRUNO ARAUJO \n\n     CRISTINA ARAUJO \n\n            RAFAEL LIMA\n\n\n\n\n\n\n\n';
+    this.func = 'Concept\nElements\n\nConcept\nCharacters \n\nPhysics \nSound Design \n\nAnimations\nGame UI\n\nProgramming\n\nGame Design\n\nArt'   
+    this.text = this.game.add.bitmapText(400, 625, 'fipps', this.team, 18);
     this.text.anchor.setTo(1,0);
     
-    this.text2 = this.game.add.bitmapText(400, 225, 'fipps', this.func, 18);
+    this.text2 = this.game.add.bitmapText(400, 625, 'fippslight', this.func, 18);
     
-        //this.game.stage.backgroundColor = "#000000";
     
     this.velocity = 0;
 
@@ -77,14 +67,19 @@ var creditsState = {
     update: function(){
       //  this.filter.update
         // Verifica se a tecla ENTER foi pressionada, utilizando o objeto relacionado do game
-        this.back.animations.play('on');
+       // this.back.animations.play('on');
       // this.hudtxt.text = 'WANDERLUST';
         
         
         
             
             if(this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+                if((this.text.body.height+this.text.body.y)>=300){
                     this.velocity=-300;
+                }
+                else{
+                    this.velocity=0;
+                }
             }
             else if(this.game.input.keyboard.isDown(Phaser.Keyboard.UP)){
                 if(this.title.body.y<=450){
@@ -94,11 +89,13 @@ var creditsState = {
                     this.velocity=0;
                 }
             }
-            else{
-                if(this.status==1){
-                    this.velocity=-100;
-                }
+            else if(this.status==1 &&(this.text.body.height+this.text.body.y>=300)){
+                this.velocity=-100;
             }
+            else if(this.status==1){
+                this.velocity=0;
+            }
+            
             
             this.title.body.velocity.setTo(0, this.velocity);
             this.sub.body.velocity.setTo(0, this.velocity);
